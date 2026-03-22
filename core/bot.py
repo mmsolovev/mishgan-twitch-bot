c6d99f78c93334582e7b25b7ffbfffa62bbf76c7
Mikhail Solovev <81823776+mmsolovev@users.noreply.github.com>
2026-03-22 23:17:06 +0300

    add: event shoutout (#8)
    Co-authored-by: Mikhail Solovev <mmsolovevmail@gmail.com>

--- core/bot.py
+++ core/bot.py
@@ -1,3 +1,7 @@
+import asyncio
+import random
+import time
+
 from twitchio.ext import commands
 from core.registry import load_commands
 from config.settings import BOT_PREFIX, TWITCH_TOKEN, TWITCH_NICK, TWITCH_CHANNEL
@@ -12,6 +16,9 @@ class Bot(commands.Bot):
             nick=TWITCH_NICK,
             initial_channels=[TWITCH_CHANNEL],
         )
+
+        self.last_shoutout = 0
+        self.recent_raids = set()
 
     async def event_ready(self):
         load_commands(self)
@@ -30,4 +37,47 @@ class Bot(commands.Bot):
         # передаём сообщение дальше,
         await self.handle_commands(message)
 
+    async def event_raw_usernotice(self, channel, tags):
+        if tags.get("msg-id") != "raid":
+            return
 
+        raider = tags.get("msg-param-login")
+        viewers = int(tags.get("msg-param-viewerCount", 0))
+
+        print(f"[RAID] {raider} with {viewers} viewers")
+
+        # фильтр
+        if not raider or viewers < 50:
+            return
+
+        # защита от дублей
+        if raider in self.recent_raids:
+            return
+
+        self.recent_raids.add(raider)
+
+        # защита от себя
+        if raider.lower() == TWITCH_CHANNEL.lower():
+            return
+
+        await self.handle_raid(raider)
+
+    async def handle_raid(self, raider: str):
+        now = time.time()
+
+        if now - self.last_shoutout < 120:
+            print("[SO] cooldown active")
+            return
+
+        delay = random.uniform(2.5, 5.5)
+        await asyncio.sleep(delay)
+
+        channel = self.get_channel(TWITCH_CHANNEL)
+        if not channel:
+            return
+
+        print(f"[SO] sending shoutout to {raider} after {delay:.2f}s")
+
+        await channel.send(f"/shoutout {raider}")
+
+        self.last_shoutout = time.time()
