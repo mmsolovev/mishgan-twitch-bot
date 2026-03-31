fd9f4bef1460dd244addf5e645e5b1ac5f04fd51
Mikhail Solovev <mmsolovevmail@gmail.com>
2026-03-31 23:55:16 +0300

    fix: модели и загрузку в sheets

--- collector/google_sheets_upload.py
+++ collector/google_sheets_upload.py
@@ -1,9 +1,9 @@
-from services.sheets_sync_service import sync_streams, sync_games
+from services.sheets_sync_service import sync_streams_safe, sync_games_safe
 
 
 def run_all():
-    sync_streams()
-    sync_games()
+    sync_streams_safe()
+    sync_games_safe()
 
 
 if __name__ == "__main__":
