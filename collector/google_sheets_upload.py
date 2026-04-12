9291819e2506430bab2456ed4e7a96d96899b1ad
Mikhail Solovev <mmsolovevmail@gmail.com>
2026-04-12 23:56:53 +0300

    fix: google sheets data sync

--- collector/google_sheets_upload.py
+++ collector/google_sheets_upload.py
@@ -9,8 +9,8 @@ from services.sheets_sync_service import (
 
 def run_all():
     # sync_bot_info()
-    sync_streams_safe()
-    sync_games_safe()
+    # sync_streams_safe()
+    # sync_games_safe()
     sync_releases_safe()
     sync_recommendations_safe()
 
